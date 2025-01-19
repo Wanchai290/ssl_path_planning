fe58b20df57215d8142379f097f2fd5270c7b0bc
Thomas Wanchai MENIER <wanchai.menier@gmail.com>
2025-01-19 13:04:44 +0100

    chore: remove unused func

--- main.py
+++ main.py
@@ -3,20 +3,8 @@ import numpy as np
 from algs.rrt import rrt, rrt_star
 
 
-def grid(pmin, pmax, step) -> ([float], [float]):
-    x = []
-    y = []
-
-    for i in np.arange(pmin, pmax + 1, step):
-        for j in np.arange(pmin, pmax + 1, step):
-            x.append(i)
-            y.append(j)
-    return x, y
-
-
 if __name__ == '__main__':
     pmin, pmax, step = (-10, 10, 0.1)
-    x, y = grid(pmin, pmax, step)
 
     start = np.array((-7.5, -6.3))
     target = np.array((8.1, 5.2))
